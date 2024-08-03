da291975337b189d8f66612c69e324e4ef2e0180
4naans <mylespillay@yahoo.co.uk>
2024-08-03 03:00:06 +0100

    latest fucking animation issues

--- src/components/DailyTrackCard.tsx
+++ src/components/DailyTrackCard.tsx
@@ -1,146 +1,3 @@
-// import React from "react";
-// import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
-// import {
-// 	View,
-// 	Text,
-// 	Image,
-// 	StyleSheet,
-// 	TouchableOpacity,
-// 	ImageSourcePropType
-// } from "react-native";
-
-// interface DailyTrackCardProps {
-// 	trackName: string;
-// 	artistName: string;
-// 	genreName: string;
-// 	artwork: ImageSourcePropType;
-// 	goToPreviousTrack: () => void;
-// 	goToNextTrack: () => void;
-// }
-
-// const DailyTrackCard = ({
-// 	trackName,
-// 	artistName,
-// 	genreName,
-// 	artwork,
-// 	goToPreviousTrack,
-// 	goToNextTrack
-// }: DailyTrackCardProps): JSX.Element => {
-// 	console.log(
-// 		trackName,
-// 		artistName,
-// 		genreName,
-// 		artwork,
-// 		"This is the passed in data for each component card "
-// 	);
-// 	function generateBackgroundColorHexCode(genreName: string) {
-// 		switch (genreName) {
-// 			case "Easy Listening":
-// 				return "#BFD7EA";
-// 			case "Garage":
-// 				return "#FFB627";
-// 			case "UK Hip-Hop":
-// 				return "#A5A299";
-// 			case "Techno":
-// 				return "#3A2449";
-// 			case "Drum & Bass":
-// 				return "#C73E1D";
-// 			default:
-// 				return "#FFFFFF"; // Default color if no match is found
-// 		}
-// 	}
-// 	function generateTextColorHexCode(genreName: string) {
-// 		switch (genreName) {
-// 			case "Easy Listening":
-// 				return "#6495ED"; // Dark Slate Gray for a soft blue background
-// 			case "Garage":
-// 				return "#C73E1D"; // Very dark gray (almost black) for bright orange
-// 			case "UK Hip-Hop":
-// 				return "#3A2449"; // Lavender for dark gray background
-// 			case "Techno":
-// 				return "#40E0D0"; // Thistle (light purple) for dark purple background
-// 			case "Drum & Bass":
-// 				return "#FFD700"; // Gold for dark red background
-// 			default:
-// 				return "#778899"; // Light Slate Gray, neutral and visible on many backgrounds
-// 		}
-// 	}
-// 	return (
-// 		<View
-// 			style={[
-// 				styles.cardContainer,
-// 				{ backgroundColor: generateBackgroundColorHexCode(genreName) }
-// 			]}>
-// 			<Image source={artwork} style={styles.artwork} />
-// 			<View style={styles.trackInfoContainer}>
-
-// 			<Text style={styles.trackName}>{trackName}</Text>
-// 			<Text style={styles.artistName}>{artistName}</Text>
-// 			<View style={styles.genreContainer}>
-// 				<TouchableOpacity onPress={goToPreviousTrack}>
-// 					<Text style={styles.navArrow}>{"<"}</Text>
-// 				</TouchableOpacity>
-// 				<Text style={[styles.genreName,{color: generateTextColorHexCode(genreName)}]}>{genreName}</Text>
-// 				<TouchableOpacity onPress={goToNextTrack}>
-// 					<Text style={styles.navArrow}>{">"}</Text>
-// 				</TouchableOpacity>
-// 				</View>
-// 			</View>
-// 		</View>
-// 	);
-// };
-
-// const styles = StyleSheet.create({
-// 	cardContainer: {
-// 		flex: 1,
-// 		padding: 16,
-// 		justifyContent: "center",
-// 		alignItems: "center",
-// 		backgroundColor: "#000", // Adjust the background color as needed
-// 		width: "100%",
-// 		height: "100%"
-// 	},
-// 	artwork: {
-// 		width: "85%",
-// 		height: "50%",
-// 		marginBottom: "10%",
-// 		borderRadius: 10,
-// 		borderColor: "#FFFFFF55",
-// 		borderWidth: 3
-// 	},
-// 	trackInfoContainer: { display: 'flex', flexDirection: 'column',  justifyContent: 'space-evenly', alignContent: 'center', alignItems: 'center', height: '25%', paddingVertical: '10%', width: '85%', borderRadius: 10, backgroundColor: '#00000055'},
-
-// 	trackName: {
-// 		fontSize: RFValue(22, 580),
-// 		fontWeight: "bold",
-// 		marginBottom: "5%",
-// 		color: "#FFFFFF" // Ensuring text color is visible on the darker background
-// 	},
-// 	artistName: {
-// 		fontSize: RFValue(16, 580),
-// 		fontWeight: "semibold",
-// 		color: "#FFFFFF",
-// 		marginBottom: "5%"
-// 	},
-// 	genreContainer: {
-// 		flexDirection: "row",
-// 		alignItems: "center",
-// 		marginTop: 8
-// 	},
-// 	genreName: {
-// 		fontSize: 24,
-// 		fontWeight: "bold",
-// 		marginHorizontal: 20,
-// 		// color: "#FFFFFF"
-// 	},
-// 	navArrow: {
-// 		fontSize: 24,
-// 		fontWeight: "bold",
-// 		color: "#FFFFFF"
-// 	}
-// });
-
-// export default DailyTrackCard;
 import React from "react";
 import {
 	View,
@@ -175,6 +32,7 @@ const DailyTrackCard = ({
 	isExpanded
 }: DailyTrackCardProps): JSX.Element => {
 	const windowHeight = Dimensions.get("window").height;
+
 	return (
 		<View
 			style={[
@@ -185,13 +43,29 @@ const DailyTrackCard = ({
 					paddingTop: windowHeight * 0.2
 				}
 			]}>
-			<Image
-				source={artwork}
-				style={[
-					styles.artwork,
-					{ height: windowHeight * 0.3, width: windowHeight * 0.3 }
-				]}
-			/>
+			<View style={styles.artworkContainer}>
+				<Image
+					source={artwork}
+					style={[
+						styles.artwork,
+						{
+							height: windowHeight * 0.3,
+							width: windowHeight * 0.3
+						}
+					]}
+				/>
+				{isExpanded && (
+					<View
+						style={[
+							styles.overlay,
+							{
+								height: windowHeight * 0.3,
+								width: windowHeight * 0.3
+							}
+						]}
+					/>
+				)}
+			</View>
 			<View style={styles.trackInfoContainer}>
 				<Text style={styles.trackName}>{trackName}</Text>
 				<Text style={styles.artistName}>{artistName}</Text>
@@ -217,11 +91,21 @@ const styles = StyleSheet.create({
 		alignContent: "center",
 		width: "100%"
 	},
+	artworkContainer: {
+		position: "relative"
+	},
 	artwork: {
 		marginBottom: "10%",
 		borderRadius: 10,
 		borderColor: "#FFFFFF55",
 		borderWidth: 3
+	},
+	overlay: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		backgroundColor: "#00000050",
+		borderRadius: 10
 	},
 	trackInfoContainer: {
 		justifyContent: "space-evenly",
@@ -230,7 +114,7 @@ const styles = StyleSheet.create({
 		paddingVertical: "10%",
 		width: "85%",
 		borderRadius: 10,
-		backgroundColor: "#00000055"
+		backgroundColor: "#00000035"
 	},
 	trackName: {
 		fontSize: RFValue(22, 580),
--- src/components/sticky-top-nav/StickyTopNavigator.tsx
+++ src/components/sticky-top-nav/StickyTopNavigator.tsx
@@ -34,7 +34,10 @@ const StickyTopNavigator = ({
 }: StickyTopNavigatorProps): JSX.Element => {
 	const topChevronPosition = useSharedValue(RFValue(5, 580));
 	const bottomChevronPosition = useSharedValue(3);
-	const translationXValue = useSharedValue(-500);
+	const translationXValue = useSharedValue(-200);
+	const nameOpacityValue = useSharedValue(0);
+	const backgroundOpacityValue = useSharedValue(0);
+	const dotOpacityValue = useSharedValue(0);
 	const windowWidth = Dimensions.get("window").width;
 	const windowHeight = Dimensions.get("window").height;
 
@@ -47,23 +50,41 @@ const StickyTopNavigator = ({
 	};
 
 	const handleExpandGenreList = () => {
-		setIsExpanded(!isExpanded);
-		topChevronPosition.value = withTiming(
-			isExpanded ? RFValue(5, 580) : RFValue(12, 580)
-		);
-		bottomChevronPosition.value = withTiming(
-			isExpanded ? RFValue(5, 580) : RFValue(12, 580)
-		);
+		if (isExpanded) {
+			// When collapsing, perform animations first
+			topChevronPosition.value = withTiming(RFValue(5, 580));
+			bottomChevronPosition.value = withTiming(RFValue(5, 580));
+			translationXValue.value = withTiming(-200, { duration: 300 });
+			backgroundOpacityValue.value = withTiming(1, { duration: 100 });
+			dotOpacityValue.value = withTiming(1, { duration: 150 });
+			nameOpacityValue.value = withTiming(0, { duration: 150 });
 
-		translationXValue.value = withDelay(
-			100,
-			withTiming(isExpanded ? -500 : 30)
-		);
+			// Set isExpanded to false after a delay to allow animations to complete
+			setTimeout(() => {
+				setIsExpanded(false);
+			}, 300);
+		} else {
+			// When expanding, set isExpanded to true first
+			setIsExpanded(true);
+			topChevronPosition.value = withTiming(RFValue(12, 580));
+			bottomChevronPosition.value = withTiming(RFValue(12, 580));
+			backgroundOpacityValue.value = withTiming(1, {
+				duration: 100
+			});
+			translationXValue.value = withTiming(15, { duration: 400 });
+			dotOpacityValue.value = withTiming(0, { duration: 100 });
+			nameOpacityValue.value = withTiming(1, { duration: 100 });
+		}
 	};
 
 	const topChevronStyle = useAnimatedStyle(() => {
 		return {
 			top: topChevronPosition.value
+		};
+	});
+	const backgroundOpacityStyle = useAnimatedStyle(() => {
+		return {
+			opacity: backgroundOpacityValue.value
 		};
 	});
 
@@ -73,19 +94,31 @@ const StickyTopNavigator = ({
 		};
 	});
 
+	const dotOpacityStyle = useAnimatedStyle(() => {
+		return {
+			opacity: dotOpacityValue.value
+		};
+	});
+
+	const nameOpacityStyle = useAnimatedStyle(() => {
+		return {
+			opacity: nameOpacityValue.value,
+			transform: [{ translateX: translationXValue.value }]
+		};
+	});
+
 	return (
-		<View
+		<Animated.View
 			style={[
 				styles.stickyHeader,
 				{
+					backgroundColor: tracks[displayedTrack]?.bgColour,
+					opacity: backgroundOpacityValue.value,
 					width: windowWidth,
 					paddingHorizontal: windowWidth * 0.02,
 					height: isExpanded
-						? windowHeight * 0.5
-						: windowHeight * 0.15,
-					backgroundColor: isExpanded
-						? tracks[displayedTrack].bgColour + "99"
-						: "#ffffff00"
+						? windowHeight * 0.9
+						: windowHeight * 0.15
 				}
 			]}>
 			<View
@@ -98,61 +131,57 @@ const StickyTopNavigator = ({
 				]}>
 				{tracks.map((track, index) =>
 					!isExpanded ? (
-						<TouchableOpacity
-							key={index}
-							disabled={index === displayedTrack ? true : false}
-							onPress={() => {
-								handleGenreDotSelect(index);
-							}}>
-							<View style={[styles.genreNavButton]}>
-								<View
-									style={[
-										styles.genreDot,
-										{
-											height:
-												index === 0
-													? 16
-													: 15 - (index + 2),
-											width:
-												index === 0
-													? 16
-													: 15 - (index + 2),
-											borderColor:
-												index === displayedTrack
-													? "goldenrod"
-													: "none",
-											borderWidth:
-												index === displayedTrack
-													? 3
-													: 0,
-											backgroundColor:
-												displayedTrack === index
-													? "#00000045"
-													: "#000000"
-										}
-									]}
-								/>
-							</View>
-						</TouchableOpacity>
-					) : (
 						<Animated.View
-							style={[
-								styles.navGenreNameContainer,
-								{
-									transform: [
-										{
-											translateX: translationXValue
-										}
-									]
-								}
-							]}>
+							key={index}
+							style={[styles.genreNavButton, dotOpacityStyle]}>
 							<TouchableOpacity
 								key={index}
+								disabled={index === displayedTrack}
+								onPress={() => {
+									handleGenreDotSelect(index);
+								}}>
+								<View style={[styles.genreNavButton]}>
+									<View
+										style={[
+											styles.genreDot,
+											{
+												height:
+													index === 0
+														? 16
+														: 15 - (index + 2),
+												width:
+													index === 0
+														? 16
+														: 15 - (index + 2),
+												borderColor:
+													index === displayedTrack
+														? "goldenrod"
+														: "none",
+												borderWidth:
+													index === displayedTrack
+														? 3
+														: 0,
+												backgroundColor:
+													displayedTrack === index
+														? "#00000045"
+														: "#000000"
+											}
+										]}
+									/>
+								</View>
+							</TouchableOpacity>
+						</Animated.View>
+					) : (
+						<Animated.View
+							key={index}
+							style={[
+								styles.navGenreNameContainer,
+								nameOpacityStyle
+							]}>
+							<TouchableOpacity
 								style={styles.navGenreTitleButton}
 								activeOpacity={0.6}
-								disabled={
-									index === displayedTrack ? true : false
-								}
+								disabled={index === displayedTrack}
 								onPress={() => {
 									handleGenreListSelection(index);
 								}}>
@@ -191,7 +220,7 @@ const StickyTopNavigator = ({
 				topChevronStyle={topChevronStyle}
 				bottomChevronStyle={bottomChevronStyle}
 			/>
-		</View>
+		</Animated.View>
 	);
 };
 const styles = StyleSheet.create({
@@ -244,7 +273,7 @@ const styles = StyleSheet.create({
 		textAlignVertical: "center",
 		textAlign: "left",
 		fontFamily: "sans-serif",
-		fontWeight: "500",
+		fontWeight: "600",
 		fontSize: 28
 	},
 	titleContainer: {
